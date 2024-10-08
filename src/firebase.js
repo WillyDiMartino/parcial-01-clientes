7532d8ce87a184f55ff8ad7d5e205e2ada2fa511
WillyDiMartino <guillermodimartinoperez@gmail.com>
2024-10-08 11:35:50 -0300

    3er prueba

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWkSy3ZXUdhxH5uTUGp_pDM3MOIo1qSas",
-  authDomain: "chat-f1-c4318.firebaseapp.com",
-  projectId: "chat-f1-c4318",
-  storageBucket: "chat-f1-c4318.appspot.com",
-  messagingSenderId: "12931774693",
-  appId: "1:12931774693:web:4eae4b73221d1cbffbbc0c",
-  measurementId: "G-JF6B05Y43G"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
